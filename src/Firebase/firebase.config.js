e7ad228251032ff69a4abe061f6290c6f5955032
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-11-07 21:58:30 +0600

    Initial setup done

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD72svRxJU-af1e1jnaJJJy9rNwzgRXlXQ",
+  authDomain: "flytographer-4cc6d.firebaseapp.com",
+  projectId: "flytographer-4cc6d",
+  storageBucket: "flytographer-4cc6d.appspot.com",
+  messagingSenderId: "931763056567",
+  appId: "1:931763056567:web:83e13d31765e37e8be0232"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
