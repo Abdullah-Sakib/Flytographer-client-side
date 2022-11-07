c03779eb293cc5a5efabc5bd63f9efde03e55d8f
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-11-07 22:38:25 +0600

    Added Footer

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     <div className="relative">
       <img
         src="https://images.unsplash.com/photo-1545232979-8bf68ee9b1af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        className="absolute inset-0 object-cover w-full h-full"
+        className="absolute inset-0 object-cover w-full h-full "
         alt=""
       />
       <div className="relative bg-opacity-75 bg-deep-purple-accent-700">
